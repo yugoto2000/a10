70161bb5854d42cb51200e66dd7b343cbaa034a9
yugoto2000 <yuhcram510s@gmail.com>
2023-08-02 19:43:02 +0900

    Update take_hist.cpp
    first_commit

---
+++ takehist/take_hist.cpp
@@ -0,0 +1,16 @@
+
+TH1D* takehist(TString option,int gate_option,int runnumber){//"up""down"option構造に
+  int runnum = 0;
+  TH1D *h;
+  TFile *file=new TFile(Form("outroot/%s/hist_run_0%d_000.root",option.Data(),runnumber));//ここでrootファイルを決める//option.Data()っていうのはoptionをchar*に変えて
+  TDirectory *dir = file->GetDirectory(Form("hTOF_Li%d_gate",gate_option));//ROOTファイル中にディレクトリがなければここはコメントアウト
+  h=(TH1D*)dir->Get(Form("hTOF_Li%d_gate_d%d",gate_option,gate_option+6));//ここはfileからgetでいいと思う
+  return h;
+}
+
+///参照方法
+///void Draw_hist(){
+///TH1D *h0;
+///h0= takeLi("up",477);////
+///h0->Draw();
+///}/////
